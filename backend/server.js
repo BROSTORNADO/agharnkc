a6dd8b28bbc5c4cd52f421f06de23edf1f9564ea
Mohamed abdallahi <97371201+BROSTORNADO@users.noreply.github.com>
2024-12-22 13:35:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,14 +35,14 @@ app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 
 //setup for deployment 
-const __dirname = path.resolve;
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// const __dirname = path.resolve;
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 
 // Start Server
