e879d8ab59f4986e6cd35a52a3eea759e0f2519e
Mohamed abdallahi <97371201+BROSTORNADO@users.noreply.github.com>
2025-06-26 06:21:55 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from '../lib/axios';
 import Slider from 'react-slick';
 import { FaMapMarkerAlt, FaWhatsapp, FaTimes } from 'react-icons/fa';
 import 'slick-carousel/slick/slick.css';
