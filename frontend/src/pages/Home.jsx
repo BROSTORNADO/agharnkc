8a27e2b359cebddd6b764a7844aec10f41c301d2
Mohamed abdallahi <97371201+BROSTORNADO@users.noreply.github.com>
2025-06-26 06:30:22 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -46,11 +46,11 @@ const Home = () => {
     try {
       let res;
       if (selectedLocation) {
-        res = await axios.get('https://agharnkc.onrender.com/api/posts', {
+        res = await axios.get('/posts', {
           params: { location: selectedLocation },
         });
       } else {
-        res = await axios.get('https://agharnkc.onrender.com/api/posts');
+        res = await axios.get('/posts');
       }
       setPosts(res.data);
     } catch (error) {
